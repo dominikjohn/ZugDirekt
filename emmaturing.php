0079877c09ac818e8813ae85493c3f9bd2a3595f
Dominik John <mail@dominikjohn.com>
2017-05-10 23:34:26 +0200

    removed second cover

--- emmaturing.php
+++ emmaturing.php
@@ -88,10 +88,8 @@ $percentage = 1 - $datediff / $datediff2;
             </div>
 
             <div class="col-xs-12 mrg">
-                <h4 class="l-header semibold dark condensed uppercase t-left">Covers</h4>
-                <img src="images/stills/et/ETcover02-min.png" width="99.99999%">
-                <br><br><br>
-                <img src="images/stills/et/ETcover01-min.png" width="100%">
+                <h4 class="l-header semibold dark condensed uppercase t-left">Cover</h4>
+                <img src="images/stills/et/ETcover02-min.png" width="100%">
             </div>
             <div class="col-xs-12 mrg">
                 <h4 class="l-header semibold dark condensed uppercase t-left">Film Stills</h4>
