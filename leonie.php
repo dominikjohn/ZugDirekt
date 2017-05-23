62bcc0abf92af2b8a94a8c2b7ae1342409a0319f
Dominik John <mail@dominikjohn.com>
2017-05-23 11:54:54 +0200

    updated timer

---
+++ leonie.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <title>ZugDirekt | Happy to say hello!</title>
+    <meta name="description" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+    <?php include 'imports.php' ?>
+</head>
+
+<body>
+
+
+<?php include 'navbar.php'; ?>
+
+<!-- Page Header -->
+<section class="page_header container waypoint">
+    <div class="page_header_inner">
+        <!-- Header -->
+        <h1 class="fixed-text hometext condensed white bold t-center">
+            Leonie
+        </h1>
+    </div>
+</section><!-- End Page Header -->
+
+
+<section id="shortcodes" class="container">
+    <!-- Pricing Tables -->
+    <div class="inner_s">
+
+        <!-- Two Column -->
+        <div class="t-left">
+            <div class="col-xs-12 mrg">
+
+                <i class="fa fa-fighter-jet fa-5x"></i>
+                <i class="fa fa-fighter-jet fa-5x fa-spin"></i>
+                <i class="fa fa-fighter-jet fa-5x"></i>
+                <i class="fa fa-fighter-jet fa-5x"></i>
+                <i class="fa fa-fighter-jet fa-5x"></i>
+                <i class="fa fa-fighter-jet fa-5x"></i>
+                <i class="fa fa-fighter-jet fa-5x"></i>
+                <i class="fa fa-fighter-jet fa-5x"></i>
+
+            </div>
+            <div class="clear"></div>
+
+        </div>
+
+
+</section><!-- End Short Codes -->
+
+
+<?php include 'footer.php'; ?>
+
+</body>
+
+</html>
