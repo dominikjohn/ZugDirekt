c4c51dc2098d21635394e0fabf2ddc37a7162bf3
Dominik John <mail@dominikjohn.com>
2017-05-23 11:56:29 +0200

    added easteregg

--- leonie.php
+++ leonie.php
@@ -2,7 +2,7 @@
 <html lang="de">
 <head>
     <meta charset="utf-8"/>
-    <title>ZugDirekt | Happy to say hello!</title>
+    <title>ZugDirekt | Leonie</title>
     <meta name="description" content=""/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <?php include 'imports.php' ?>
