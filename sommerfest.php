e56d0e8c0922470913b9ac987094a5d88534d062
Dominik John <dominikjohn@users.noreply.github.com>
2017-05-31 01:08:53 +0200

    Update sommerfest.php

--- sommerfest.php
+++ sommerfest.php
@@ -29,8 +29,8 @@
 
         <!-- Header -->
         <h1 class="header semibold white title">Rückmeldung zum ZugDirekt Sommerfest</h1>
-	<h2 class="semibold white title">27. Juni 2017, 20 Uhr</h4>
-	<h2 class="semibold white title">Giesinger Bahnhofsplatz 1, 81539 München</h4>
+	<h2 class="semibold white">27. Juni 2017, 20 Uhr</h2>
+	<h2 class="semibold white">Giesinger Bahnhofsplatz 1, 81539 München</h2>
         <!-- Description -->
         <h4 class="h-desc white contact-text"></h4>
 
