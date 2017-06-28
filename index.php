22b068e42984e91bff0c1e9a873295cfe5649ebd
Dominik John <mail@dominikjohn.com>
2017-06-28 14:37:11 +0200

    ET release [2]

--- index.php
+++ index.php
@@ -54,11 +54,11 @@
     <!-- Team Inner -->
     <div class="inner team">
         <!--  DeHeader -->
-        <h1 class="header semibold dark title">Emma Turing</h1>
+        <h1 class="header semibold dark title">Das neueste Projekt</h1>
 
         <!--scription -->
-        <h4 class="dark">Das neueste Projekt<span class="dark bold"></span>
-        </h4>
+<!--        <h4 class="dark">Das neueste Projekt<span class="dark bold"></span>-->
+<!--        </h4>-->
         <!-- Members -->
         <div class="team-members inner-details">
             <!-- Member -->
